766b28538556893f0ad5b9520419d69ce0e23e8e
Balamurugan K <138365153+Bala-273@users.noreply.github.com>
2025-02-09 21:46:02 +0530

    Update FinalKeywordDemo.java

--- Task_2/Final_Keyword/FinalKeywordDemo.java
+++ Task_2/Final_Keyword/FinalKeywordDemo.java
@@ -2,10 +2,10 @@ package task_2;
 
 
 final class FinalClass {
-    final int finalVariable = 100;
+    final int finalVariable = 157;
 
     public final void finalMethod() {
-        System.out.println("This is a final method in the FinalClass.");
+        System.out.println("This is the final method in the FinalClass.");
     }
 
     public void display() {
@@ -18,7 +18,7 @@ public class FinalKeywordDemo {
     public static void main(String[] args) {
         FinalClass finalClass = new FinalClass();
 
-        System.out.println("Accessing final variable: " + finalClass.finalVariable);
+        System.out.println("Accessing final variable value: " + finalClass.finalVariable);
 
         finalClass.finalMethod();
     }
