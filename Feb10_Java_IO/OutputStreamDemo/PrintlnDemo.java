c63a3cec6e1709abbcb037faddf7cceb85b028fd
Balamurugan K <138365153+Bala-273@users.noreply.github.com>
2025-02-11 17:59:02 +0530

    Add files via upload

---
+++ Feb10_Java_IO/OutputStreamDemo/PrintlnDemo.java
@@ -0,0 +1,8 @@
+package Java.Io_10_feb;
+
+public class PrintlnDemo {
+    public void MethodForPrintln(){
+        System.out.println("This is statement 1");
+        System.out.println("This is statement 2");
+    }
+}
