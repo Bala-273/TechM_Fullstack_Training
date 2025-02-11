b4c026add85eaa5e99df40c2b0a8b6b881a746c3
Balamurugan K <138365153+Bala-273@users.noreply.github.com>
2025-02-11 17:58:06 +0530

    Delete Feb10_Java_IO/PrintErrorDemo.java

--- Feb10_Java_IO/PrintErrorDemo.java
+++
@@ -1,8 +0,0 @@
-package Java.Io_10_feb;
-
-public class PrintErrorDemo {
-    public static void MethodForPrinterr() {
-        System.err.print("This is an error message using System.err.print.\n");
-        System.err.println("This is an error message using System.err.println.");
-    }
-}
