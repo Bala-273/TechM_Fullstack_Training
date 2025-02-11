ab08914f89be536c0d4742e0f0d3f0f7d2daf038
Balamurugan K <138365153+Bala-273@users.noreply.github.com>
2025-02-11 17:58:18 +0530

    Delete Feb10_Java_IO/PrintlnDemo.java

--- Feb10_Java_IO/PrintlnDemo.java
+++
@@ -1,8 +0,0 @@
-package Java.Io_10_feb;
-
-public class PrintlnDemo {
-    public void MethodForPrintln(){
-        System.out.println("This is statement 1");
-        System.out.println("This is statement 2");
-    }
-}
