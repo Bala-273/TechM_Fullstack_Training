0687bb6636b5839bb6686c685541c1b817ffb067
Bala-273 <balak271103@gmail.com>
2025-03-10 21:19:57 +0530

    React_Task

--- React_Task/React_Task/vite-project/src/Feb28/Error_Boundaries/ABComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ABComponentForError() {
-    const undefinedVariable = undefined;
-    console.log(undefinedVariable.abc())
-  return (
-    <h3>ABComponentForError</h3>
-  )
-}
